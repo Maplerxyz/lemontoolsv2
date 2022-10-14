ceb0fa409456703d00838bcbc69825691ce456e4
Mapler <76017463+Maplerxyz@users.noreply.github.com>
2022-10-14 16:28:37 +1100

    Edit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ bot = discord.Bot()
 @bot.event
 async def on_ready():
     print(f"Bot logged in as {bot.user}!")
+    print(f"Run /hello to test if the bot works.")
 
 @bot.slash_command(guild_ids=[your, guild_ids, here])
 async def hello(ctx):
